3f10c744161562edaf4553ba8526f0703f2ede5d
sunitee <suniteebajpai2@gmail.com>
2021-03-03 16:45:39 -0500

    Completed Assignment

---
+++ index.js
@@ -0,0 +1,55 @@
+function add() {   
+    console.log (2, 5);
+}
+
+function subtract() {   
+    console.log (8, 5);
+}
+
+function multiply() {   
+    console.log (3, 5);
+}
+
+function divide() {   
+    console.log (10, 5);
+}
+
+
+function add( a, b){
+    return( a + b);
+}
+
+function subtract( b, a){
+    return( b - a );
+}
+
+function multiply( a, b){
+    return( a * b);
+}
+
+function divide( a, b){
+    return( a / b);
+}
+
+function increment(n){
+    return(++n) ;
+}
+
+function decrement(n){
+    return(--n) ;
+}
+
+function makeInt(n){
+    return parseInt(n);
+}
+
+function makeInt(n,base) {
+    return parseInt(n,10);
+ }
+
+function preserveDecimal(n){
+     return parseFloat(n);
+ }
+
+
+
